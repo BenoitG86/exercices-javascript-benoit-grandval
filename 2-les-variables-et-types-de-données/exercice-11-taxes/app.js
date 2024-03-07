0b616b17bddc88c117ec4c66794ea95d44d8e467
Benoît <bgrandval@deastanceservices.fr>
2024-03-07 15:00:25 +0100

    Create & mod

--- 2-les-variables-et-types-de-données/exercice-11-taxes/app.js
+++ 2-les-variables-et-types-de-données/exercice-11-taxes/app.js
@@ -1,12 +1,13 @@
 let prixHT = Number(prompt("Entrez ici le prix HT de votre item"));
 document.write(``);
 
-let taux = 1.15;
-let prixTTC = prixHT * taux;
+let taux = 15;
+let taxe = (taux/100)+1;
+let prixTTC = Math.round(prixHT * taxe);
 
 document.write(
       `Le prix HT est de ${prixHT}. <br>
-      Le prix TTC est de ${prixTTC} pour un taux de ${taux}.`);
+      Le prix TTC est de ${prixTTC} pour un taux de ${taux}%.`);
 
 /* Résultat attendu dans la console, pour un prixHT de 100€ : 
 prix HT : 100 €

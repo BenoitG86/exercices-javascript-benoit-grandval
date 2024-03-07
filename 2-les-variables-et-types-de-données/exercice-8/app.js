5dc36deab84ace15ca3be586bba1f364f95ab616
Benoît <bgrandval@deastanceservices.fr>
2024-03-07 12:16:06 +0100

    Mod

---
+++ 2-les-variables-et-types-de-données/exercice-8/app.js
@@ -0,0 +1,3 @@
+const bigNumber = BigInt(999_999_999_999_999);
+
+console.log(`Le type de "bigNumber" est ${typeof bigNumber} et il a comme valeur ${bigNumber}`);
---
+++ 2-les-variables-et-types-de-données/exercice-9/app.js
@@ -0,0 +1,8 @@
+let mot = prompt("Choissisez un mot qui vous fait kiffer la vibe :")
+
+document.write(`Le mot ${mot} contient très exactement ${mot.length} lettres ( à une ou deux vaches près) !`);
+/* Résultat attendu pour le mot "tomate", dans la console :
+
+"tomate" contient 6 lettres
+
+*/

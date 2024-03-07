35f738e3d6d1f58752ac57c3862b9cdb6fb3947b
Benoît <bgrandval@deastanceservices.fr>
2024-03-07 11:55:10 +0100

    Create & mod

--- 2-les-variables-et-types-de-données/exercice-7/app.js
+++ 2-les-variables-et-types-de-données/exercice-7/app.js
@@ -1,15 +1,27 @@
-let age = 25;
-let genre = "femme";
-let estContent = true;
+let firstNumber = Number(prompt("Une idée de chiffre ?"));
+let secondNumber = Number(prompt("Une idée d'un AUTRE chiffre peut-être ?"));
 
-console.log(typeof age);
-console.log(typeof genre);
-console.log(typeof estContent);
+let addition = firstNumber + secondNumber;
+let soustraction = firstNumber - secondNumber;
+let multiplication = firstNumber * secondNumber;
+let division = firstNumber / secondNumber;
+let exposant = firstNumber ** secondNumber;
 
-/* Résultat attendu dans la console : 
+console.log(
+      `Le résultat est 
+      ${addition} pour l'addition,
+      ${soustraction} pour la soustraction,
+      ${multiplication} pour une multiplication,
+      ${division} pour la fameuse division et
+      ${exposant} pour un exposant, même si je sais pas ce que c'est !`
+);
 
-La variable "age" est de type : number
-La variable "genre" est de type : string
-La variable "estContent" est de type : boolean
+/* Résultat attendu pour 10 et 2, dans la console :
+
+10 x 2 = 20
+10 - 2 = 8
+10 + 2 = 12
+10 / 2 = 5
+10 exposant 2 = 100
 
 */

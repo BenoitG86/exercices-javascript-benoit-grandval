ad46f666dc0905328035ab4248b55daa0edfcdbb
Benoît <bgrandval@deastanceservices.fr>
2024-03-13 14:35:57 +0100

    Create

---
+++ 4-boucles/exercice-1-incrementation/app.js
@@ -0,0 +1,11 @@
+for (let index = 0; index <= 10; index++) {
+      document.write(`${index} <br>`);
+}
+
+// Résultat attendu dans la console :
+
+// 0
+// 1
+// 2
+// ...
+// 10
---
+++ 4-boucles/exercice-2-lettrage/app.js
@@ -0,0 +1,8 @@
+let mot = prompt("Insérez un mot de votre choix ( mais pas de vulgarité SVP! ) :")
+
+/* Résultat attendu pour le mot "html", dans la console :
+
+h
+t
+m
+l */

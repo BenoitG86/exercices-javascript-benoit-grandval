3692e1060e0a0c3e0c601efce991ab9d815f62aa
Benoît <bgrandval@deastanceservices.fr>
2024-03-15 15:00:31 +0100

    Create and mod

---
+++ 5-tableaux/exercice-1-selection/app.js
@@ -0,0 +1,12 @@
+const nombres = [12, 18, 33, 5, 78, 56, 47, 28]
+
+document.write(`Le premier nombre du tableau est ${nombres[0]} <br>
+Le troisième nombre du tableau est ${nombres[2]} <br>
+Le dernier nombre du tableau est ${nombres[nombres.length-1]}`);
+
+
+// // Résultat dans la console :
+
+// le premier nombre du tableau est 12
+// le troisième nombre du tableau est 33
+// le dernier nombre du tableau est 28
---
+++ 5-tableaux/exercice-2-longueur/app.js
@@ -0,0 +1,12 @@
+const couleurs = ['bleu', 'rouge', 'vert', 'jaune']
+
+document.write(couleurs.length);
+couleurs =+ ['violet' , 'blouge'];
+document.write(couleurs.length);
+
+/* 
+Créez un tableau contenant plusieurs couleurs. 
+Affichez la longueur du tableau dans la console, puis 
+ajoutez deux couleurs supplémentaires et affichez 
+à nouveau la longueur pour voir le changement.
+*/

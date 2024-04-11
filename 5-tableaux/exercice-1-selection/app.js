79c5e39dc8a04d5c3719267164b2cfbf9fde0ed2
Benoît <bgrandval@deastanceservices.fr>
2024-04-11 13:29:00 +0200

    Mod

--- 5-tableaux/exercice-1-selection/app.js
+++ 5-tableaux/exercice-1-selection/app.js
@@ -1,8 +1,10 @@
 const nombres = [12, 18, 33, 5, 78, 56, 47, 28]
 
-document.write(`Le premier nombre du tableau est ${nombres[0]} <br>
-Le troisième nombre du tableau est ${nombres[2]} <br>
-Le dernier nombre du tableau est ${nombres[nombres.length-1]}`);
+console.log(
+      `Le premier nombre du tableau est ${nombres[0]} <br>
+      Le troisième nombre du tableau est ${nombres[2]} <br>
+      Le dernier nombre du tableau est ${nombres[nombres.length-1]}`
+);
 
 
 // // Résultat dans la console :

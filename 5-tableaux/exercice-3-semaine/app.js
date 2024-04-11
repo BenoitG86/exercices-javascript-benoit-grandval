d90bd0de11d1ae9e4081952e6475b591374cb5ac
Benoît <bgrandval@deastanceservices.fr>
2024-04-11 13:49:08 +0200

    Mod

--- 5-tableaux/exercice-3-semaine/app.js
+++ 5-tableaux/exercice-3-semaine/app.js
@@ -1,9 +1,12 @@
 let week = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
 
+
+
 for (let index = 0; index < week.length; index++) {
 
       if (index === week.length - 1) {
             document.write(`Le jour ${index + 1} de la semaine est ${week[index]} et le ${week[index]} c'est gratuit. <br>`);
+
       } else {
             document.write(`Le jour ${index + 1} de la semaine est ${week[index]}<br>`);
       }
--- 5-tableaux/exercice-5-patates/app.js
+++ 5-tableaux/exercice-5-patates/app.js
@@ -1,9 +1,14 @@
 let week = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+let index = 0;
 
 // console.log (week.length);
 // console.table (week);
 
 for (let day of week) {
-      // if (day === week.length)
-      console.log(`${day}, des patates`)
+      if (index < 6) {
+            console.log(`${day}, des patates`)
+            index++;
+      } else {
+            console.log(`${day}, des patates, encore et encore`)
+      }
 }
